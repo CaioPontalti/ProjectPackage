e33e56367527071398bf5d236e820ae6279ff97b
Caio Pontalti Almeida <5by5.pontalti@voeazul.com.br>
2025-07-26 16:34:02 -0300

    useCaseTest Login

--- src/BackEnd/Tests/CommonTestUtilities/Entities/UserMock.cs
+++ src/BackEnd/Tests/CommonTestUtilities/Entities/UserMock.cs
@@ -4,14 +4,23 @@ namespace CommonTestUtilities.Entities;
 
 public static class UserMock
 {
-    public static User CreateUserActiveBuilder()
+    public static User CreateUserActiveBuilder(string? passwordHash = null)
     {
+        if (string.IsNullOrEmpty(passwordHash))
+            return new Faker<User>()
+                .RuleFor(user => user.Id, f => f.Random.Guid().ToString())
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.PasswordHash, (f) => f.Internet.Password(8))
+                .RuleFor(user => user.IsActive, f => true);
+
         return new Faker<User>()
-            .RuleFor(user => user.Id, f => f.Random.Guid().ToString())
-            .RuleFor(user => user.Name, (f) => f.Person.FirstName)
-            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
-            .RuleFor(user => user.PasswordHash, (f) => f.Internet.Password(8))
-            .RuleFor(user => user.IsActive, f => true);
+                .RuleFor(user => user.Id, f => f.Random.Guid().ToString())
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.PasswordHash, (f) => passwordHash)
+                .RuleFor(user => user.IsActive, f => true);
+
     }
 
     public static User CreateUserInactiveBuilder()
---
+++ src/BackEnd/Tests/UseCases.Test/Auth/Login/LoginUseCaseTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Requests.Auth;
+using Moq;
+using Project.Application.UseCases.Auth.Login;
+using Project.Domain.Interfaces.Repositories;
+using Project.Domain.Interfaces.Services;
+using System.Net;
+
+namespace UseCases.Test.Auth.Login;
+
+public class LoginUseCaseTest
+{
+    private readonly Mock<IUserRepository> _userRepository = new Mock<IUserRepository>();
+    private readonly Mock<IAuthService> _authService = new Mock<IAuthService>();
+
+    [Fact]
+    public async Task LoginUseCase_WhenLoginIsSuccess_ReturnsSuccess()
+    {
+        var request = LoginRequestBuilder.BuildSuccess();
+
+        request.Validate();
+
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.Password);
+        var user = UserMock.CreateUserActiveBuilder(hashedPassword);
+        var token = new Project.Domain.ValueObjects.Auth.Token { AcccessToken = "ceruivhuifhviuthiuthvrt" };
+
+        var useCase = CreateUseCase();
+        _userRepository
+            .Setup(rep => rep.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(user);
+
+        _authService
+            .Setup(auth => auth.GenerateToken(user))
+            .Returns(token);
+
+        var result = await useCase.ExecuteAsync(request);
+        Assert.Empty(result.Errors);
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Data.User);
+        Assert.NotNull(result.Data.Token);
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+
+
+
+
+    private LoginUseCase CreateUseCase()
+    {
+        return new LoginUseCase(_userRepository.Object, _authService.Object);
+    }
+}
